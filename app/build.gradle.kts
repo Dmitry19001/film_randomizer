87125b60aa16ecf1dd4fa66c26a3ec13a8f3a610
Dmitry19001 <dmitry.gongalo@student.lab.fi>
2023-11-08 13:59:10 +0200

    dependencies cleanup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,31 +53,32 @@ android {
 }
 
 dependencies {
+    // Core
     implementation("androidx.core:core-ktx:1.9.0")
+
+    // Basic Layout
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Settings
     implementation("androidx.preference:preference-ktx:1.2.0")
 
     // Coroutines
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
 
-    //MaterialDesign
+    //MaterialDesign + Compose
     implementation ("androidx.compose.material3:material3:1.1.2")
     implementation ("androidx.compose.material3:material3-window-size-class:1.1.2")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
 
     // REST Client
     implementation("io.ktor:ktor-client-core:2.3.5")
     implementation("io.ktor:ktor-client-okhttp:2.3.5")
-
-
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.7.2")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
