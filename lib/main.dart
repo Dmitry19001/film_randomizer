4acc90dd3b2ccb6a94d135045fb3dd0d57c09ae0
Dmitry19001 <dmitry.gongalo@student.lab.fi>
2024-03-08 21:26:23 +0200

    added support for film provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:film_randomizer/providers/film_provider.dart';
 import 'package:film_randomizer/ui/screens/home_page.dart';
 import 'package:film_randomizer/ui/themes/default.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:logger/logger.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,16 +15,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      onGenerateTitle: (BuildContext context) {
-        final title = AppLocalizations.of(context)!.app_title;
-        Logger().d("App title is: $title");
-        return title;
-      },
-      theme: AppTheme.defaultTheme,
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      home: const MyHomePage(),
+    return ChangeNotifierProvider(
+      create: (context) => FilmProvider(),
+      child: MaterialApp(
+        onGenerateTitle: (BuildContext context) {
+          final title = AppLocalizations.of(context)!.app_title;
+          Logger().d("App title is: $title");
+          return title;
+        },
+        theme: AppTheme.defaultTheme,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        home: const MyHomePage(),
+      ),
     );
   }
 }
--- lib/ui/screens/home_page.dart
+++ lib/ui/screens/home_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
+import 'package:film_randomizer/models/film.dart';
+import 'package:film_randomizer/providers/film_provider.dart'; 
+import 'package:film_randomizer/ui/widgets/film_detail_widget.dart'; 
+import 'package:film_randomizer/generated/localization_accessors.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -9,40 +13,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
+    // Accessing the FilmProvider
+    final filmProvider = Provider.of<FilmProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(AppLocalizations.of(context)!.app_title),
+        title: Text(L10nAccessor.get(context, 'app_title')), // Assuming L10nAccessor setup is correctly done.
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: filmProvider.films != null
+          ? ListView.builder(
+              itemCount: filmProvider.films!.length,
+              itemBuilder: (context, index) {
+                Film film = filmProvider.films!.elementAt(index);
+                return FilmDetailWidget(film: film);
+              },
+            )
+          : Center(child: CircularProgressIndicator()), // Show loading indicator while films are loading
     );
   }
 }
