321262973d02375c220c70f44a66f70802e1102c
Dmitry19001 <dmitry.gongalo@student.lab.fi>
2024-03-09 16:14:33 +0200

    blank page

---
+++ lib/ui/screens/randomizer_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RandomizeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Randomize Screen"),
+      ),
+      body: Center(
+        child: Text("This is the Randomize Screen"),
+      ),
+    );
+  }
+}
