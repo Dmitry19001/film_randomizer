d1c30a04446c7f09aedc6b0632b3844d76438a0e
Dmitry19001 <dmitry.gongalo@student.lab.fi>
2024-03-16 17:42:08 +0200

    auth provider

---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,58 @@
+import 'package:film_randomizer/providers/settings_provider.dart';
+import 'package:film_randomizer/services/auth_service.dart';
+import 'package:flutter/material.dart';
+
+class AuthProvider with ChangeNotifier {
+  final AuthService _authService = AuthService();
+  String? _username;
+  String? _token;
+
+  String? get username => _username;
+  String? get token => _token;
+
+  bool get isAuthenticated => _username != null && _token != null;
+
+  AuthProvider() {
+    loadAuthData();
+  }
+
+  Future<bool> loadAuthData() async {
+    final authData = await SettingsProvider.loadAuthData();
+
+    if (authData["username"] == null 
+      || authData["username"]!.isEmpty) return false;
+
+    _username = authData["username"];
+    _token = authData["token"];
+    notifyListeners();
+
+    return true;
+  }
+
+  Future<bool> login(String login, String password) async {
+    Map<String, String> userData = {
+      'username': login,
+      'password':password
+    };
+    _token = await _authService.login(userData);
+
+    if (_token != null)
+    {
+      SettingsProvider.saveAuthData(username: login, token: _token!);
+      notifyListeners();
+
+      return true;
+    }
+
+    return false;
+  }
+
+  Future<void> logout(String login, String password) async {
+    _token = null;
+    _username = null;
+    
+    SettingsProvider.saveAuthData();
+
+    notifyListeners();
+  }
+}
