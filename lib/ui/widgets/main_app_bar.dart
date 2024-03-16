9e225e11d32a5826067f2227796d3e8516186c61
Dmitry19001 <dmitry.gongalo@student.lab.fi>
2024-03-16 18:55:33 +0200

    customizable title for main app bar

--- lib/ui/widgets/main_app_bar.dart
+++ lib/ui/widgets/main_app_bar.dart
@@ -3,13 +3,16 @@ import 'package:film_randomizer/generated/localization_accessors.dart';
 import 'package:film_randomizer/ui/screens/settings_page.dart';
 
 class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const MainAppBar({super.key});
+  final String? title;
+  const MainAppBar({super.key, this.title});
 
   @override
   Widget build(BuildContext context) {
+    final String fallBackTitle = L10nAccessor.get(context, 'app_title');
+
     return AppBar(
       backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      title: Text(L10nAccessor.get(context, 'app_title')),
+      title: Text(title ?? fallBackTitle),
       actions: <Widget>[
         IconButton(
           icon: const Icon(Icons.settings),
