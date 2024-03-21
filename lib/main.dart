3de2b44abad83472e483fc1b76bd95c5293b759a
Dmitry19001 <dmitri.gongalo@gmail.com>
2024-03-21 17:23:40 +0200

    simplified auth check logic

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,8 @@ import 'package:film_randomizer/providers/category_provider.dart';
 import 'package:film_randomizer/providers/film_provider.dart';
 import 'package:film_randomizer/providers/genre_provider.dart';
 import 'package:film_randomizer/providers/settings_provider.dart';
-import 'package:film_randomizer/ui/screens/film_add_edit_page.dart';
 import 'package:film_randomizer/ui/screens/home_page.dart';
 import 'package:film_randomizer/ui/screens/login_register_page.dart';
-import 'package:film_randomizer/ui/screens/randomizer_page.dart';
-import 'package:film_randomizer/ui/screens/settings_page.dart';
 import 'package:film_randomizer/ui/themes/dark.dart';
 import 'package:film_randomizer/ui/themes/default.dart';
 import 'package:flutter/material.dart';
@@ -36,68 +33,35 @@ void main() async {
   );
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   final AuthProvider authProvider;
   final SettingsProvider settingsProvider;
   const MyApp({super.key, required this.authProvider, required this.settingsProvider});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider.value(value: widget.authProvider),
-        ChangeNotifierProvider.value(value: widget.settingsProvider),
         ChangeNotifierProvider(create: (context) => FilmProvider()),
         ChangeNotifierProvider(create: (context) => GenreProvider()),
         ChangeNotifierProvider(create: (context) => CategoryProvider()),
       ],
       child: MaterialApp(
-        locale: widget.settingsProvider.language,
+        locale: settingsProvider.language,
         onGenerateTitle: (BuildContext context) {
           final title = AppLocalizations.of(context)!.app_title;
           return title;
         },
-        theme: widget.settingsProvider.theme == AppTheme.dark ? DarkTheme.themeData : DefaultTheme.themeData,
+        theme: settingsProvider.theme == AppTheme.dark ? DarkTheme.themeData : DefaultTheme.themeData,
         localizationsDelegates: AppLocalizations.localizationsDelegates,
         supportedLocales: AppLocalizations.supportedLocales,
-        home: _getInitialScreen(widget.authProvider),
-        onGenerateRoute: _onGenerateRoute,
+        home: _getInitialScreen(authProvider),
       ),
     );
   }
 
   Widget _getInitialScreen(AuthProvider auth) {
     return auth.isAuthenticated ? const HomePage() : const LoginRegisterPage();
-  }
-
-  Route? _onGenerateRoute(RouteSettings settings) {
-    if (!widget.authProvider.isAuthenticated) {
-      return MaterialPageRoute(builder: (ctx) => const LoginRegisterPage());
-    }
-
-    var routeBuilders = _buildRoutes();
-
-    WidgetBuilder? builder = routeBuilders[settings.name];
-    if (builder != null) {
-      return MaterialPageRoute(builder: builder, settings: settings);
-    }
-
-    return MaterialPageRoute(builder: (ctx) => const HomePage());
-  }
-
-  Map<String, WidgetBuilder> _buildRoutes() {
-    return {
-      HomePage.routeName: (ctx) => const HomePage(),
-      SettingsScreen.routeName: (ctx) => const SettingsScreen(),
-      FilmEditPage.routeName: (ctx) => const FilmEditPage(),
-      LoginRegisterPage.routeName: (ctx) => const LoginRegisterPage(),
-      RandomizeScreen.routeName: (ctx) => const RandomizeScreen(),
-    };
   }
 }
 
