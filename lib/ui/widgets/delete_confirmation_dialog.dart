b38654179b9624aa83c381318d733a0cb18d8f23
Dmitry19001 <dmitri.gongalo@gmail.com>
2024-03-22 10:02:44 +0200

    deletion confirmation dialog

---
+++ lib/ui/widgets/delete_confirmation_dialog.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:film_randomizer/generated/localization_accessors.dart';
+
+Future<bool> showDeleteConfirmationDialog(BuildContext context) async {
+  return (await showDialog<bool>(
+    context: context,
+    builder: (context) => AlertDialog(
+      title: Text(L10nAccessor.get(context, "confirmation")),
+      content: Text(L10nAccessor.get(context, "delete_confirmation_title")),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(false),
+          child: Text(L10nAccessor.get(context, "cancel")),
+        ),
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(true),
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.resolveWith((states) {
+              return Colors.red;
+            }),
+          ),
+          child: Text(L10nAccessor.get(context, "delete")),
+        ),
+      ],
+    ),
+  )) ?? false;
+}
