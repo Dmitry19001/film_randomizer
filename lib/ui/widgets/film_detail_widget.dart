9a32a1af0fe5f1a9ebc4a2fe809035b6f4baa674
Dmitry19001 <dmitri.gongalo@gmail.com>
2024-03-22 10:26:39 +0200

    implemented film deletion

--- lib/ui/widgets/film_detail_widget.dart
+++ lib/ui/widgets/film_detail_widget.dart
@@ -1,7 +1,11 @@
 import 'package:film_randomizer/generated/localization_accessors.dart';
+import 'package:film_randomizer/providers/film_provider.dart';
 import 'package:film_randomizer/ui/screens/film_add_edit_page.dart';
+import 'package:film_randomizer/ui/widgets/delete_confirmation_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:film_randomizer/models/film.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:provider/provider.dart';
 
 
 class FilmDetailWidget extends StatefulWidget {
@@ -91,19 +95,12 @@ class _FilmDetailWidgetState extends State<FilmDetailWidget> {
         child: Container(
           color: Colors.black54,
           child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
+            child: Wrap(
+              // mainAxisSize: MainAxisSize.min,
+              spacing: 10,
               children: [
-                ElevatedButton(
-                  onPressed: () => {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => FilmEditPage(film: widget.film,)),
-                    ),
-                    _toggleOverlay()
-                  },
-                  child: Text(L10nAccessor.get(context, "edit")),
-                ),
+                _buildDeleteButton(context),
+                _buildEditButton(context),
               ],
             ),
           ),
@@ -111,7 +108,52 @@ class _FilmDetailWidgetState extends State<FilmDetailWidget> {
       ),
     );
   }
-  
+
+  ElevatedButton _buildEditButton(BuildContext context) {
+    return ElevatedButton.icon(
+      onPressed: () => {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => FilmEditPage(film: widget.film,)),
+        ),
+        _toggleOverlay()
+      },
+      icon: const Icon(Icons.edit_document),
+      label: Text(L10nAccessor.get(context, "edit")),
+    );
+  }
+
+  IconButton _buildDeleteButton(BuildContext context) {
+    return IconButton(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.resolveWith((states) {
+          return Colors.red;
+        }),
+      ),
+      onPressed: () async {
+        await _handleDelete(context);
+      },
+      icon: const Icon(Icons.delete),
+      tooltip: L10nAccessor.get(context, "delete"),
+    );
+  }
+
+  Future<void> _handleDelete(BuildContext context) async {
+    _toggleOverlay();
+    
+    final bool confirmDelete = await showDeleteConfirmationDialog(context);
+    
+    if (!confirmDelete) return;
+
+    final result = await Provider.of<FilmProvider>(context, listen: false).deleteFilm(widget.film);
+    
+    if (!mounted) return;
+
+    final message = L10nAccessor.get(context, result ? "film_delete_success" : "film_delete_error");
+
+    Fluttertoast.showToast(msg: message);
+  }
+
   void _toggleOverlay() {
     setState(() {
       _isOverlayVisible = !_isOverlayVisible;
