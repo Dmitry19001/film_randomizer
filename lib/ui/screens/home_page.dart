de9b102194fa03b0c8e3e558a9e966f89285cfd1
Dmitry19001 <dmitri.gongalo@gmail.com>
2024-06-02 13:13:37 +0300

    fixed watched state filtering

--- lib/ui/screens/home_page.dart
+++ lib/ui/screens/home_page.dart
@@ -3,7 +3,6 @@ import 'package:film_randomizer/ui/screens/film_add_edit_page.dart';
 import 'package:film_randomizer/ui/widgets/bottom_navigation_widget.dart';
 import 'package:film_randomizer/ui/widgets/main_app_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:logger/logger.dart';
 import 'package:provider/provider.dart';
 import 'package:film_randomizer/models/film.dart';
 import 'package:film_randomizer/providers/film_provider.dart'; 
@@ -18,42 +17,59 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late final FilmProvider _filmProvider;
+  Iterable<Film>? _films;
+
+  @override
+  void initState() {
+    _filmProvider = Provider.of<FilmProvider>(context, listen: false);
+
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) async => await _syncFilms());
+  }
+
   @override
   Widget build(BuildContext context) {
-    // Accessing the FilmProvider
-    final filmProvider = Provider.of<FilmProvider>(context);
-    final settingsProvider = Provider.of<SettingsProvider>(context);
-
-    if (!settingsProvider.showWatched) filmProvider.filterWatched();
-
     return Scaffold(
       appBar: const MainAppBar(),
-      body: filmProvider.films != null
-          ? ListView.builder(
-              itemCount: filmProvider.films!.length,
-              itemBuilder: (context, index) {
-                Film film = filmProvider.films!.elementAt(index);
-                return FilmDetailWidget(film: film);
-              },
-            )
-          : const Center(child: CircularProgressIndicator()),
+      body: Consumer<SettingsProvider>(
+        builder: (context, settingsProvider, child) {
+          _syncFilms(settingsProvider.showWatched);
+          return _films != null
+              ? ListView.builder(
+                  itemCount: _films!.length,
+                  itemBuilder: (context, index) {
+                    Film film = _films!.elementAt(index);
+                    return FilmDetailWidget(film: film);
+                  },
+                )
+              : const Center(child: CircularProgressIndicator());
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () => {
           Navigator.push(
             context,
-            MaterialPageRoute(builder: (context) => FilmEditPage(film: Film(),)),
+            MaterialPageRoute(builder: (context) => FilmEditPage(film: Film())),
           )
         },
       ),
       bottomNavigationBar: CustomBottomNavigation(
-        onSync: () async => {_syncFilms(filmProvider)},
+        onSync: () async => {_syncFilms()},
       ),
     );
   }
-  
-  void _syncFilms(FilmProvider filmProvider) async {
-    Logger().d("Sync");
-    await filmProvider.loadFilms();
+
+  Future<void> _syncFilms([bool? showWatched]) async {
+    await _filmProvider.loadFilms();
+
+    if (showWatched != null && !showWatched) {
+      await _filmProvider.filterWatched();
+    }
+
+    setState(() {
+      _films = _filmProvider.films;
+    });
   }
 }
