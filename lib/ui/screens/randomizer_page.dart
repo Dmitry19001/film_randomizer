9da6c62d3bdeedaeb6dd0ef34208701c42817754
Dmitry Gongalo <dmitri.gongalo@gmail.com>
2024-08-23 13:47:59 +0300

    Added film filtering, improved randomizer ui

--- lib/ui/screens/randomizer_page.dart
+++ lib/ui/screens/randomizer_page.dart
@@ -1,11 +1,18 @@
+import 'dart:async';
 import 'dart:math';
+import 'package:film_randomizer/models/base/localizable.dart';
+import 'package:film_randomizer/models/category.dart';
+import 'package:film_randomizer/models/genre.dart';
+import 'package:film_randomizer/providers/category_provider.dart';
+import 'package:film_randomizer/providers/genre_provider.dart';
+import 'package:film_randomizer/ui/widgets/film_detail_widget.dart';
+import 'package:film_randomizer/ui/widgets/multi_select_field.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:provider/provider.dart';
 import 'package:film_randomizer/models/film.dart';
-import 'package:film_randomizer/providers/film_provider.dart';
 import 'package:film_randomizer/util/random_utilities.dart';
 import 'package:film_randomizer/generated/localization_accessors.dart';
+import 'package:provider/provider.dart';
 import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 class RandomizeScreen extends StatefulWidget {
@@ -20,26 +27,52 @@ class RandomizeScreen extends StatefulWidget {
 }
 
 class _RandomizeScreenState extends State<RandomizeScreen> with SingleTickerProviderStateMixin {
-  final ItemScrollController itemScrollController = ItemScrollController();
-  final ScrollOffsetController scrollOffsetController = ScrollOffsetController();
-  final ItemPositionsListener itemPositionsListener = ItemPositionsListener.create();
-  final ScrollOffsetListener scrollOffsetListener = ScrollOffsetListener.create();
+  final ItemScrollController _itemScrollController = ItemScrollController();
+  final ScrollOffsetController _scrollOffsetController = ScrollOffsetController();
+  final ItemPositionsListener _itemPositionsListener = ItemPositionsListener.create();
+  final ScrollOffsetListener _scrollOffsetListener = ScrollOffsetListener.create();
+  
   Iterable<Film> _films = [];
-  int? selectedFilmIndex;
+  Iterable<Film> _filteredFilms = [];
+  Iterable<Genre> _genres = [];
+  Iterable<Category> _categories = [];
+
+  List<Genre> _genresToFilter = [];
+  List<Category> _categoriesToFilter = [];
+
+  int? _selectedFilmIndex;
+
+  bool _includeWathced = false;
+  bool _genresFilterIncludeMode = true;
+  bool _categoriesFilterIncludeMode = true;
+
 
   @override
   void initState() {
     super.initState();
-    
+    _loadInitialData();
+  }
+
+  void _loadInitialData() async {
+    final genreProvider = Provider.of<GenreProvider>(context, listen: false);
+    final categoryProvider = Provider.of<CategoryProvider>(context, listen: false);
+
+    await genreProvider.loadGenres();
+    await categoryProvider.loadCategories();
+     
     setState(() {
+      _genres = genreProvider.genres;
+      _categories = categoryProvider.categories;
       _films = widget.films ?? [];
     });
+
+    _filterFilms(_films);
   }
 
   @override
   Widget build(BuildContext context) {
     if (widget.films == null || widget.films!.isEmpty) {
-      Fluttertoast.showToast(msg: "Add films first!");
+      Fluttertoast.showToast(msg: L10nAccessor.get(context, "missing_films"));
       Navigator.of(context).pop();
     }
 
@@ -49,34 +82,161 @@ class _RandomizeScreenState extends State<RandomizeScreen> with SingleTickerProv
       ),
       body: Column(
         children: [
-          _buildControls(),
           Expanded(
-            flex: 5,
+            flex: 2,
             child: _buildFilmRandomList(),
           ),
+          _buildFilterControls(),
         ],
       ),
       floatingActionButton: _buildRandomizeButton(),
     );
   }
 
-  Widget _buildControls() {
-    return Container();
+  Widget _buildFilterControls() {
+    return Expanded(
+      flex: 5,
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Column(
+            children: [
+              CheckboxMenuButton(
+                value: _includeWathced,
+                onChanged: (include) {
+                  setState(
+                  () {
+                    _includeWathced = include!;
+                  });
+                  _filterFilms(_films);
+                },
+                child: Text(
+                  L10nAccessor.get(context, "include_watched"),
+                ),
+              ),
+              ... _buildMultiSelectFields(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _buildMultiSelectFields() {
+    return [
+      const SizedBox(height: 8),
+      if (_genres.isNotEmpty)
+        _buildFilterRow(
+          selectedItems: _genresToFilter,
+          includeMode: _genresFilterIncludeMode,
+          toggleIncludeMode: () => setState(() => _genresFilterIncludeMode = !_genresFilterIncludeMode),
+          clearSelectedItems: () {
+            setState(() {
+              _genresToFilter = [];
+            });
+            _filterFilms(_films);
+          },
+          multiselect: MultiSelectField<Genre>(
+            context: context,
+            items: _genres.toList(),
+            title: L10nAccessor.get(context, "genres"),
+            buttonText: L10nAccessor.get(context, "select_genres"),
+            selectedItems: _genresToFilter,
+            onConfirm: (items) {
+              setState(() {
+                _genresToFilter = items.toList();
+              });
+              _filterFilms(_films);
+            },
+            onChipTap: (item) {
+              setState(() {
+                _genresToFilter.remove(item);
+                _genresToFilter = _genresToFilter.toList(); //Fixing chip not removing bug
+              });
+              _filterFilms(_films);
+            },
+            buttonIconData: Icons.album,
+          ),
+        ),
+      const SizedBox(height: 8),
+      if (_categories.isNotEmpty)
+        _buildFilterRow(
+          selectedItems: _categoriesToFilter,
+          includeMode: _categoriesFilterIncludeMode,
+          toggleIncludeMode: () => setState(() => _categoriesFilterIncludeMode = !_categoriesFilterIncludeMode),
+          clearSelectedItems: () {
+            setState(() {
+              _categoriesToFilter = [];
+            });
+            _filterFilms(_films);
+          },
+          multiselect: MultiSelectField<Category>(
+            context: context,
+            items: _categories.toList(),
+            title: L10nAccessor.get(context, "categories"),
+            buttonText: L10nAccessor.get(context, "select_categories"),
+            selectedItems: _categoriesToFilter,
+            onConfirm: (items) {
+              setState(() {
+                _categoriesToFilter = items.toList();
+              });
+              _filterFilms(_films);
+            },
+            onChipTap: (item) {
+              setState(() {
+                _categoriesToFilter.remove(item);
+                _categoriesToFilter = _categoriesToFilter.toList(); //Fixing chip not removing bug
+              });
+              _filterFilms(_films);
+            },
+            buttonIconData: Icons.category,
+          ),
+        ),
+    ];
+  }
+
+  Widget _buildFilterRow({
+    required bool includeMode,
+    required MultiSelectField multiselect,
+    required VoidCallback toggleIncludeMode,
+    required List<Localizable> selectedItems,
+    required VoidCallback clearSelectedItems,
+  }) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        IconButton(
+          icon: Icon(includeMode ? Icons.add : Icons.remove),
+          color: includeMode ? Colors.green : Colors.red,
+          onPressed: toggleIncludeMode,
+          tooltip: L10nAccessor.get(context, "switch_include_mode"),
+        ),
+        Expanded(
+          child: multiselect,
+        ),
+        if (selectedItems.isNotEmpty)
+          IconButton(
+            icon: const Icon(Icons.delete),
+            onPressed: clearSelectedItems,
+            color: Colors.red,
+          ),
+      ],
+    );
   }
 
   Widget _buildFilmRandomList() {
     return Stack(
       children: [
         ScrollablePositionedList.builder(
-          itemCount: _films.length + 1,
-          itemScrollController: itemScrollController,
-          scrollOffsetController: scrollOffsetController,
-          itemPositionsListener: itemPositionsListener,
-          scrollOffsetListener: scrollOffsetListener,
+          itemCount: _filteredFilms.length + 1,
+          itemScrollController: _itemScrollController,
+          scrollOffsetController: _scrollOffsetController,
+          itemPositionsListener: _itemPositionsListener,
+          scrollOffsetListener: _scrollOffsetListener,
           physics: const BouncingScrollPhysics(),
           itemBuilder: (context, index) {
-            if (index < _films.length) {
-              Film film = _films.elementAt(index);
+            if (index < _filteredFilms.length) {
+              Film film = _filteredFilms.elementAt(index);
               return _buildFilmListEntry(index, film);
             } else {
               return SizedBox(
@@ -91,7 +251,7 @@ class _RandomizeScreenState extends State<RandomizeScreen> with SingleTickerProv
   }
 
   Container _buildFilmListEntry(int index, Film film) {
-    bool isSelected = index == selectedFilmIndex;
+    bool isSelected = index == _selectedFilmIndex;
     return Container(
       padding: const EdgeInsets.all(8.0),
       decoration: isSelected
@@ -109,9 +269,6 @@ class _RandomizeScreenState extends State<RandomizeScreen> with SingleTickerProv
       child: Text(
         film.title!,
         textAlign: TextAlign.center,
-        // style: TextStyle(
-        //   color: isSelected ? Theme.of(context).primaryColor : null,
-        // ),
       ),
     );
   }
@@ -129,7 +286,7 @@ class _RandomizeScreenState extends State<RandomizeScreen> with SingleTickerProv
                 stops: const [0.3, 0.4],
                 colors: <Color>[
                   Colors.transparent,
-                  Theme.of(context).primaryColor,
+                  Theme.of(context).scaffoldBackgroundColor,
                 ],
                 tileMode: TileMode.mirror,
               ),
@@ -138,7 +295,7 @@ class _RandomizeScreenState extends State<RandomizeScreen> with SingleTickerProv
         ),
         Expanded(
           child: Container(
-            color: Theme.of(context).primaryColor,
+            color: Theme.of(context).scaffoldBackgroundColor,
           ),
         ),
       ],
@@ -155,18 +312,89 @@ class _RandomizeScreenState extends State<RandomizeScreen> with SingleTickerProv
   }
 
   void _handleRandomize() {
+    _itemScrollController.jumpTo(index: 0);
+
+    _filterFilms(_films);
+
     setState(() {
-      _films = randomizeSize(_films);
-      selectedFilmIndex = Random().nextInt(_films.length);
+      _filteredFilms = randomizeSize(_filteredFilms);
     });
-    if (selectedFilmIndex != null) {
+
+    _selectedFilmIndex = Random().nextInt(_filteredFilms.length);
+    if (_selectedFilmIndex! < (_filteredFilms.length / 2).ceil() ) {
+      // Value should be always outside of 1/2 of the list, minimal repeats of films are 3 times
+      // This makes the rolling animation not too slow
+      _selectedFilmIndex = _selectedFilmIndex! + (_filteredFilms.length / 2).ceil();
+    }
+
+    if (_selectedFilmIndex != null) {
       WidgetsBinding.instance.addPostFrameCallback((_) {
-        itemScrollController.scrollTo(
-          index: selectedFilmIndex!,
+        _itemScrollController.scrollTo(
+          index: _selectedFilmIndex!,
           duration: const Duration(seconds: 2),
           curve: Curves.easeInOutSine,
         );
+
+        Timer(const Duration(milliseconds: 2200), _showFilmDetails);
       });
     }
+  }
+
+  void _showFilmDetails() {
+    showGeneralDialog(
+      context: context,
+      pageBuilder: (context, animation, secondaryAnimation) {
+        return Container();
+      },
+      transitionBuilder: (context, animation, secondaryAnimation, child){
+        return ScaleTransition(
+          scale: Tween<double>(begin: 0.1, end: 1.0).animate(animation),
+          child: FilmDetailWidget(
+            film: _filteredFilms.elementAt(_selectedFilmIndex!),
+            showAdditionalControls: true
+          ),
+        );
+      },
+    );
+  }
+  
+  void _filterFilms(Iterable<Film> films) {
+    if (films.isEmpty) return;
+
+    List<Film> temp = films.toList();
+
+    // filtering out films that are watched
+    if (!_includeWathced){
+      temp = temp.where((film) => film.isWatched == false).toList();
+    }
+
+    // filtering by genres
+    if (_genresToFilter.isNotEmpty) {
+      temp = temp.where(
+        (film) => _genresToFilter.any(
+          (genre) => _genresFilterIncludeMode ?
+            film.genres.contains(genre) // include
+            : !film.genres.contains(genre) // exclude
+        )
+      ).toList();
+    }
+
+    // filtering by categories
+    if (_categoriesToFilter.isNotEmpty) {
+      temp = temp.where(
+        (film) => _categoriesToFilter.any(
+          (category) => _categoriesFilterIncludeMode ?
+            film.categories.contains(category) // include
+            : !film.categories.contains(category) // exclude
+        )
+      ).toList();
+    }
+
+    setState(() {
+      _selectedFilmIndex = null;
+      _filteredFilms = temp;
+    });
+
+    _itemScrollController.jumpTo(index: 0);
   }
 }
